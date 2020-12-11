43b7148943e1b299bf0ca375d8bc41e521e12082
DanEricson <danericsonil@gmail.com>
2020-12-11 10:49:55 -0600

    Finished? cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of functions below solve the problem of time-intensive matrix
+## inversion calculations, where a matrix object is created and cached 
+## in the first function, and can be calculated if needed or inverted in the 
+## second function
+
+## All matrices inverted by these functions are assumed to be square and 
+## invertable
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+    inv <- NULL
   
-  set <- function(a_Matrix) {
-    x <<- a_Matrix
-    inv <<- NULL
-  }
+    set <- function(a_Matrix) {
+      x <<- a_Matrix
+      inv <<- NULL
+    }
   
-  get <- function() x
-  
+    get <- function() x
+    setInverse <- function(ginv) inv <<- ginv
+    getInverse <- function() inv
+    
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
